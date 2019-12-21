57e0d478f5e4680e85373e97b422d825297ebe62
LF112 <lf@lf112.net>
2019-12-21 18:42:02 +0800

    build
    假的，这是构建耗的版本，用于预览。

---
+++ docs/endit.js
@@ -0,0 +1,60 @@
+module.exports = {
+    index: [
+        [
+            'lcookie',
+            '2019, 3月11日',
+            '轻量级的 Cookie 同意组件。',
+            '由 LF112 创建',
+            'https://i.loli.net/2019/12/21/dfckZwSt253QUln.png',
+            9
+        ],
+        [
+            'licount',
+            '2019, 3月14日',
+            '轻量而简单的计数组件。',
+            '由 LF112 创建',
+            'https://i.loli.net/2019/12/21/tvFr4DVjO2yQiCT.png',
+            1
+        ],
+        [
+            '深空矩阵',
+            '2019, 3月16日',
+            '由 @ExDragine 的组织 深空矩阵 的名字有感而发。',
+            '送给 ExDragine 的',
+            'https://i.loli.net/2019/12/21/3plv41shkYUwQxb.png',
+            6
+        ],
+        [
+            'BTCO',
+            '2019, 4月11日',
+            '宝塔面板响应式解决方案，为解决移动端访问面板而生。',
+            '与 野狼君 协作',
+            'https://cdn.lfio.net/FURRY/Lenowolf_2.jpg',
+            226
+        ],
+        [
+            'Yume',
+            '2019, 9月27日',
+            '送给好朋友的国庆礼物。',
+            '送给 Akiyama_yume 的',
+            'https://i.loli.net/2019/12/21/AzFJKP6pSZohGd5.png',
+            2
+        ],
+        [
+            'FURRY.ICU',
+            '2019, 10月24日',
+            '自己设计开发的个人主页!',
+            '由 LF112 创建',
+            'https://i.loli.net/2019/12/21/xlmPajSsAQDVwug.png',
+            6
+        ],
+        [
+            'Endit',
+            '2019, 12月21日',
+            '年末时光轴，记录今年开源的项目。',
+            '由 LF112 创建',
+            'https://i.loli.net/2019/12/21/pmY8owJjK16ybXv.png',
+            '?'
+        ]
+    ]
+}
